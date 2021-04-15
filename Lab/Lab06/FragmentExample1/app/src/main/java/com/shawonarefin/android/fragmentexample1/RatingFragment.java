30b4b4e6c02f0b6eadfd623fe9a914b944910515
Mahmud1431000642 <79445401+mahmudNSUCSE@users.noreply.github.com>
2021-04-16 03:54:27 +0600

    Designed fragment_rating.xml

---
+++ Lab/Lab06/FragmentExample1/app/src/main/java/com/shawonarefin/android/fragmentexample1/RatingFragment.java
@@ -0,0 +1,17 @@
+package com.shawonarefin.android.fragmentexample1;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+
+
+
