2d43681f8cec4bb808a66792f36c02274a7aa4bd
Mahmud1431000642 <79445401+mahmudNSUCSE@users.noreply.github.com>
2021-05-10 22:43:15 +0600

    Commit: Added Firebase Login

---
+++ Theory/Assignment/Assignment03/StudentDatabase/app/src/main/java/com/shawonarefin/android/roomdbv05/uicontroller/AddStudentPersonalinfoActivity.java
@@ -0,0 +1,84 @@
+package com.shawonarefin.android.roomdbv05.uicontroller;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.shawonarefin.android.roomdbv05.R;
+import com.shawonarefin.android.roomdbv05.datasource.local.room.entity.StudentInfo;
+import com.shawonarefin.android.roomdbv05.viewmodel.StudentInfoViewModel;
+
+import java.util.Date;
+
+public class AddStudentPersonalinfoActivity extends AddStudentActivity implements AppCompatActivity {
+
+
+    Spinner schoolSpinner, departmentSpinner;
+    //DatePicker dob = findViewById(R.id.editTextDate);
+    EditText nid = findViewById(R.id.nid_text);
+
+    schoolSpinner = findViewById(R.id.school_spinner);
+    departmentSpinner = findViewById(R.id.department_spinner)
+
+    Button nextButton;
+
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+// School and Department choosing through Spinner method
+        private void populateSpinnerSchool() {
+            ArrayAdapter <String> schoolAdapter = new ArrayAdapter<>(this,
+                    android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.school_spinner));
+
+            schoolAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            schoolSpinner.setAdapter(schoolAdapter);
+        }
+        private void populateSpinnerDepartment() {
+
+            ArrayAdapter <String> departmentAdapter = new ArrayAdapter<>(this,
+                    android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.department_spinner));
+
+            departmentAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            departmentSpinner.setAdapter(departmentAdapter);
+
+        }
+
+
+        Button nextButton = findViewById(R.id.next_button);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String sspinner = schoolSpinner.getSelectedItem().toString();
+
+                String dspinner = departmentSpinner.getSelectedItem().toString();
+
+                if(nid)
+
+                Intent intent = new Intent(AddStudentPersonalinfoActivity.this, AddStudentContactActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+
+}
---
+++ Theory/Assignment/Assignment03/StudentDatabase/app/src/main/java/com/shawonarefin/android/roomdbv05/uicontroller/DisplayStudentIDsActivity.java
@@ -0,0 +1,115 @@
+package com.shawonarefin.android.roomdbv05.uicontroller;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.shawonarefin.android.roomdbv05.R;
+import com.shawonarefin.android.roomdbv05.datasource.local.room.entity.StudentInfo;
+import com.shawonarefin.android.roomdbv05.viewmodel.StudentInfoViewModel;
+
+import java.util.List;
+
+public class DisplayStudentIDsActivity
+    extends AppCompatActivity
+    implements StudentIDRecyclerAdapter.OnStudentIDListener{
+
+  public static String STUDENT_DATA = "STUDENT_DATA";
+  public static String STUDENT_ID = "STUDENT_ID";
+
+  private RecyclerView mRecyclerView;
+  private StudentIDRecyclerAdapter mAdapter;
+  private FloatingActionButton mFabButton;
+
+  private StudentInfoViewModel mWordViewModel;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_display_student_ids);
+
+    mWordViewModel = (new ViewModelProvider(this
+        ,ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication()))).get(StudentInfoViewModel.class);
+
+    mRecyclerView = findViewById(R.id.recyclerview);
+    mAdapter = new StudentIDRecyclerAdapter(this
+        , mWordViewModel.getAllStudents().getValue());
+
+    mWordViewModel.getAllStudents().observe(this, new Observer<List<StudentInfo>>() {
+      @Override
+      public void onChanged(List<StudentInfo> students) {
+        mAdapter.setStudentIDList(students);
+        mAdapter.notifyDataSetChanged();
+      }
+    });
+
+    Button searchButton = findViewById(R.id.button_search);
+    searchButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent searchIntent = new Intent(DisplayStudentIDsActivity.this, SearchActivity.class);
+        Intent(searchIntent);
+      }
+    });
+
+
+    mRecyclerView.setAdapter(mAdapter);
+    mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    mFabButton = findViewById(R.id.list_add_fab_button);
+    mFabButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent intent = new Intent(getBaseContext(), AddStudentActivity.class);
+        startActivity(intent);
+      }
+    });
+
+    ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,
+        ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+      @Override
+      public boolean onMove(@NonNull RecyclerView recyclerView,
+                            @NonNull RecyclerView.ViewHolder viewHolder,
+                            @NonNull RecyclerView.ViewHolder target) {
+        return false;
+      }
+
+      @Override
+      public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder
+          , int direction) {
+        int position = viewHolder.getAdapterPosition();
+        StudentInfo infoToDelete = mAdapter.getStudentInfoAtPosition(position);
+        mWordViewModel.delete(infoToDelete);
+      }
+    });
+    helper.attachToRecyclerView(mRecyclerView);
+  }
+
+  @Override
+  public void onStudentIDClick(int position) {
+    Integer idVal  = mAdapter.getStudentID(position);
+    if(idVal == null){
+      Toast.makeText(this, "Error",Toast.LENGTH_LONG).show();
+    }else{
+      Intent intent = new Intent(this, EditStudentInfoActivity.class);
+      StudentInfo studentInfo = mWordViewModel.getStudent(idVal).getValue();
+      intent.putExtra(STUDENT_ID, idVal);
+      startActivity(intent);
+    }
+
+  }
+
+
+
+}
