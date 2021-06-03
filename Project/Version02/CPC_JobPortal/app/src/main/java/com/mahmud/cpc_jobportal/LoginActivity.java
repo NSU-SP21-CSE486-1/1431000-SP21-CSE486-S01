4c79aa394d085f5994cd050c153dc8333a1a0a99
Mahmud1431000642 <79445401+mahmudNSUCSE@users.noreply.github.com>
2021-06-03 22:57:53 +0600

    Implementing on the Version02
    Implementing the project. This made an error on the Gradle build on Version01.

---
+++ Project/Version02/CPC_JobPortal/app/src/main/java/com/mahmud/cpc_jobportal/LoginActivity.java
@@ -0,0 +1,49 @@
+package com.mahmud.cpc_jobportal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText email, password;
+
+    private Button btn_login;
+    private Button btn_register;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Created Separate Function for code readability
+        LoginFunction();
+    }
+
+    private void LoginFunction(){
+        email = findViewById(R.id.email_login);
+        password = findViewById(R.id.password_login);
+
+        btn_login = findViewById(R.id.button_login);
+        btn_register = findViewById(R.id.button_register);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
+
+    }
+}
